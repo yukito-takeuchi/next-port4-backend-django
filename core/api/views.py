16332218274298c76c0a81804f8234a475f9bfd1
yukito <iyggf66974@yahoo.ne.jp>
2025-03-10 10:20:13 +0900

    CreatePage ok

--- core/api/views.py
+++ core/api/views.py
@@ -4,8 +4,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from ..models import Post
 from .serializers import DeviceSerializer
+from rest_framework.views import APIView 
 
+from rest_framework.response import Response
+from rest_framework import status
 
 class DeviceAPIView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = DeviceSerializer
+
+class UserCreateView(APIView):
+    def post(self, request):
+        serializer = DeviceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # モデルインスタンスを保存
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
