10c6c6e8449ff90b21146fa216078879a030844b
yukito <iyggf66974@yahoo.ne.jp>
2025-03-10 10:41:48 +0900

    UpdatePage error

--- core/api/views.py
+++ core/api/views.py
@@ -20,3 +20,17 @@ class UserCreateView(APIView):
             serializer.save()  # モデルインスタンスを保存
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserUpdateView(APIView):
+    def put(self, request, pk):
+        try:
+            user = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = DeviceSerializer(user, data=request.data)  # 既存のインスタンスを渡す
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
