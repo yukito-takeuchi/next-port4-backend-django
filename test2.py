85f09e0f44b9c5d068db46d943f53b31cc537649
yukito <iyggf66974@yahoo.ne.jp>
2025-03-11 17:49:02 +0900

    data ok

--- test2.py
+++ test2.py
@@ -5,33 +5,35 @@ amazonURL = 'https://scraping-for-beginner.herokuapp.com/ranking/'
 amazonPage = requests.get(amazonURL)
 soup = BeautifulSoup(amazonPage.text, "html.parser")
 
-content = soup.find_all('div', class_='u_areaListRankingBox row')
+data = []
+spots = soup.find_all('div', class_='u_areaListRankingBox row')
 # get_a = content.find_all('a')
-spot = content[0]
-spot_name = spot.find(class_='u_title col s12')
-spot_name.find('span').extract()
-spot_name = spot_name.text.replace('\n','')
-# print(spot_name)
-eval_num = float(spot.find(class_='evaluateNumber').text )
-# print(eval_num)
+for spot in spots:
+    spot_name = spot.find(class_='u_title col s12')
+    spot_name.find('span').extract()
+    spot_name = spot_name.text.replace('\n','')
+    # print(spot_name)
+    eval_num = float(spot.find(class_='evaluateNumber').text )
+    # print(eval_num)
 
-categoryItems = spot.find(class_='u_categoryTipsItem col s12')
-categoryItems = categoryItems.find_all('dl')
+    categoryItems = spot.find(class_='u_categoryTipsItem col s12')
+    categoryItems = categoryItems.find_all('dl')
 
-# categoryItem = categoryItems[0]
-# rank = categoryItem.dd.text
-# print(rank)
-# category = categoryItem.dt.text
-# print(category)
+    # categoryItem = categoryItems[0]
+    # rank = categoryItem.dd.text
+    # print(rank)
+    # category = categoryItem.dt.text
+    # print(category)
 
-details = {}
-for categoryItem in categoryItems:
-    rank = float(categoryItem.dd.text)
-    category = categoryItem.dt.text
-    details[category] = rank
+    details = {}
+    for categoryItem in categoryItems:
+        rank = float(categoryItem.dd.text)
+        category = categoryItem.dt.text
+        details[category] = rank
 
-detum = details 
-detum['観光地名'] = spot_name
-detum['評点'] = eval_num
+    detum = details 
+    detum['観光地名'] = spot_name
+    detum['評点'] = eval_num
+    data.append(detum)
 
-print(detum)
+print(data)
