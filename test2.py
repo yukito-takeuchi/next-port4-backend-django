a2d35bd3e0413668f9b766ca9319c64e078b8927
yukito <iyggf66974@yahoo.ne.jp>
2025-03-11 16:18:43 +0900

    rank category ok

--- test2.py
+++ test2.py
@@ -7,5 +7,18 @@ soup = BeautifulSoup(amazonPage.text, "html.parser")
 
 content = soup.find_all('div', class_='u_areaListRankingBox row')
 # get_a = content.find_all('a')
-print(content[0])
+spot = content[0]
+# print(spot)
+
+eval_num = spot.find('span', class_='evaluateNumber').text
+# print(eval_num)
+
+categoryItems = spot.find(class_='u_categoryTipsItem col s12')
+categoryItems = categoryItems.find_all('dl')
+categoryItem = categoryItems[0]
+# print(categoryItem)
+rank = categoryItem.dd.text
+print(rank)
+category = categoryItem.dt.text
+print(category)
 
