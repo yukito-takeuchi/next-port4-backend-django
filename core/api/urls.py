66a50c03e17400dba3393dec1176bb67644beca0
yukito <iyggf66974@yahoo.ne.jp>
2025-03-12 19:16:48 +0900

    scrapeAndApi スクレイピング　URL入力　ok

--- core/api/urls.py
+++ core/api/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
   path('device/', views.TaskCreateListAPIView.as_view()),
   path('device/<int:pk>/', views.TaskRetrieveUpdataDestroyAPIView.as_view()),
+  path('jobs/', views.scrape_and_api),
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -7,14 +7,14 @@ from ..models import Post, Job
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
- 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class TaskCreateListAPIView(views.APIView):
   """ Taskモデルの登録API """
   def post(self, request, *args, **kwargs):
-    # Serializerを作成
+    # JSON文字列をレスポンスとして返す
     serializer = DeviceSerializer(data=request.data)
     # バリデーション実行
     serializer.is_valid(raise_exception=True)
@@ -26,11 +26,23 @@ class TaskCreateListAPIView(views.APIView):
   def get(self, request, *args, **kwargs):
     """ Taskモデルの一覧取得API """
     # 複数のobjectの場合、many=Trueを指定します
-    jobs = scrape_and_save()
+    jobs = scrape(request.data)
     return Response(jobs, status.HTTP_200_OK)
 
 
 
+@csrf_exempt
+def scrape_and_api(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        try:
+            jobs = scrape(url)
+            data = {'jobs': jobs}
+            return JsonResponse(data)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    else:
+            return JsonResponse({'error': 'POSTリクエストのみ受け付けます'}, status=405)
 
 class TaskRetrieveUpdataDestroyAPIView(views.APIView):
   """ Taskモデルのpk APIクラス """
@@ -68,6 +80,40 @@ class TaskRetrieveUpdataDestroyAPIView(views.APIView):
 
 def scrape_and_save():
     amazonURL = 'https://scraping-for-beginner.herokuapp.com/ranking/'
+
+    amazonPage = requests.get(amazonURL)
+    soup = BeautifulSoup(amazonPage.text, "html.parser")
+    data = []
+    spots = soup.find_all('div', class_='u_areaListRankingBox row')
+    # get_a = content.find_all('a')
+    for spot in spots:
+        spot_name = spot.find(class_='u_title col s12')
+        spot_name.find('span').extract()
+        spot_name = spot_name.text.replace('\n','')
+        rank = float(spot.find(class_='evaluateNumber').text )
+
+
+        categoryItems = spot.find(class_='u_categoryTipsItem col s12')
+        categoryItems = categoryItems.find_all('dl')
+
+        details = {}
+        for categoryItem in categoryItems:
+            rank = float(categoryItem.dd.text)
+            category = categoryItem.dt.text
+            details[category] = rank
+
+        detum = details
+        detum['観光地名'] = spot_name
+        detum['評点'] = rank
+        data.append(detum)
+        # print(details['楽しさ'])
+        # jobs = Job.objects.create(spot = detum['観光地名'], rank = detum['評点'], acucess = detum['アクセス'], fun = detum['楽しさ'], many = detum['人混みの多さ'], view = detum['景色'])
+ 
+    return data
+    # print(data[0]['楽しさ'])
+
+def scrape(url):
+    amazonURL = url
 
     amazonPage = requests.get(amazonURL)
     soup = BeautifulSoup(amazonPage.text, "html.parser")
