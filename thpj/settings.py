bdd7e0b3bb027e8fd9f1f63048d6bbd70602bd3e
yukito <iyggf66974@yahoo.ne.jp>
2025-03-14 13:17:40 +0900

    Create Update error 修正

--- thpj/settings.py
+++ thpj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #追加
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 追加
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Next.jsアプリケーションのURL
+]
