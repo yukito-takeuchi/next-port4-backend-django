1f641f498973adc07b7d4323d975e0828cc7ce87
yukito <iyggf66974@yahoo.ne.jp>
2025-03-17 12:10:13 +0900

    deploy

--- thpj/settings.py
+++ thpj/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import environ
-from decouple import config
-from dj_database_url import parse as dburl
+import dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
